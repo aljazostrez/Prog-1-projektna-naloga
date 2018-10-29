cb01ff76603c6babc93a6344d29267d05e25c960
aljazostrez <42022276+aljazostrez@users.noreply.github.com>
2018-10-29 14:32:56 +0100

    csv

--- Obdelava_podatkov.py
+++ Obdelava_podatkov.py
@@ -8,9 +8,9 @@ vzorec_bloka = re.compile(
     flags=re.DOTALL
 )
 
-with open('stran-1.html', 'r') as dat:
-    niz = dat.read()
-
+#with open('stran-1.html', 'r') as dat:
+#    niz = dat.read()
+#
 #l = []
 #for ujemanje in vzorec_bloka.finditer(niz):
 #    oglas = ujemanje.group(0)
@@ -32,48 +32,58 @@ podatki_avtomobila = re.compile(
 
 
 def izloci_podatke_avtomobila(blok):
-    avto = podatki_avtomobila.search(blok).groupdict()
-    avto['model'] = avto['model'].split(' ')[1]
-    if 'registracije' in avto['letnik']:
-        letnica = avto['letnik'][avto['letnik'].index(':')+1:]
-        avto['letnik'] = int(letnica)
-    else:
-        avto['letnik'] = 'NOVO'
-    avto['prevozeni_kilometri'] = int(avto['prevozeni_kilometri'])
-    avto['gorivo'] = avto['gorivo'].split(' ')[0]
-    if 'bencin' in avto['gorivo']:
-        avto['gorivo'] = 'bencin'
-    else:
-        avto['gorivo'] = 'dizel'
-    avto['prostornina_v_ccm'] = int(avto['prostornina_v_ccm'])
-    avto['moc_v_kW'] = int(avto['moc_v_kW'])
-    if avto['menjalnik'][0] == 'r':
-        avto['menjalnik'] = 'rocni'
-    else:
-        avto['menjalnik'] = 'avtomatski'
-    if '.' in avto['cena_v_evrih']:
-        avto['cena_v_evrih'] = avto['cena_v_evrih'].replace('.', '')
-    avto['cena_v_evrih'] = int(avto['cena_v_evrih'])
-    return avto
+    try:
+        avto = podatki_avtomobila.search(blok).groupdict()
+        avto['model'] = avto['model'].split(' ')[1]
+        if 'registracije' in avto['letnik']:
+            letnica = avto['letnik'][avto['letnik'].index(':')+1:]
+            avto['letnik'] = int(letnica)
+        else:
+            avto['letnik'] = 'NOVO'
+        avto['prevozeni_kilometri'] = int(avto['prevozeni_kilometri'])
+        avto['gorivo'] = avto['gorivo'].split(' ')[0]
+        if 'bencin' in avto['gorivo']:
+            avto['gorivo'] = 'bencin'
+        else:
+            avto['gorivo'] = 'dizel'
+        avto['prostornina_v_ccm'] = int(avto['prostornina_v_ccm'])
+        avto['moc_v_kW'] = int(avto['moc_v_kW'])
+        if avto['menjalnik'][0] == 'r':
+            avto['menjalnik'] = 'rocni'
+        else:
+            avto['menjalnik'] = 'avtomatski'
+        if '.' in avto['cena_v_evrih']:
+            avto['cena_v_evrih'] = avto['cena_v_evrih'].replace('.', '')
+        avto['cena_v_evrih'] = int(avto['cena_v_evrih'])
+        return avto
+    except:
+        return None
 
 
 def avtomobili_na_strani(st_strani):
     ime_datoteke = 'stran-{}.html'.format(st_strani)
-    vsebina = orodja.vsebina_datoteke(ime_datoteke)
+    niz = orodja.vsebina_datoteke(ime_datoteke)
     avtomobili = []
-    for blok in vzorec_bloka.finditer(vsebina):
-        avtomobil = blok.group(0)
-        if len(avtomobil) > 20000:
-        # Problem je le prvi avtomobil. Da se izognemo
-        # nadaljnih težav, ga takoj uredimo.
-            avtomobil = avtomobil[26150:]
-        avtomobili.append(izloci_podatke_avtomobila(avtomobil))
+    for ujemanje in vzorec_bloka.finditer(niz):
+        oglas = ujemanje.group(0)
+        if len(oglas) > 20000:
+            # Problem je le prvi oglas. Da se izognemo
+            # nadaljnih težav, ga takoj uredimo.
+            oglas = oglas[26150:]
+        avtomobili.append(oglas)
     return avtomobili
 
 
 avtomobili = []
 for st_strani in range(1, 22):
     for avto in avtomobili_na_strani(st_strani):
-        avtomobili.append(avto)
+        podatki = izloci_podatke_avtomobila(avto)
+        if podatki != None:
+            avtomobili.append(izloci_podatke_avtomobila(avto))
 
-
+avtomobili.sort(key=lambda y: y['prevozeni_kilometri'])
+orodja.zapisi_csv(
+    avtomobili, ['model', 'letnik', 'prevozeni_kilometri',
+    'gorivo', 'prostornina_v_ccm', 'moc_v_kW', 'menjalnik',
+    'cena_v_evrih'], 'obdelani-podatki/avtomobili.csv'
+    )
